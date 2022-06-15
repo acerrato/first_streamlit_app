38aefcd2ca8f2299ec6a0677fed3dfd5ee02acac
Anthony Cerrato <anthonycerrato@gmail.com>
2022-06-15 05:01:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit
 
 streamlit.title("My Parents Healthy New Diner")
 
-streamlit.header("Breakfast Menu")
-
-streamlit.text("Omega 3 Oatmeal")
-streamlit.text("Kale Juice")
-streamlit.text("Hard Boiled Eggs")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
